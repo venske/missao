c5fc9341c284f339f921e75dfac8e607dd015090
venske <105080911+venske@users.noreply.github.com>
2024-08-29 10:38:16 -0300

    kk

--- script.js
+++ script.js
@@ -47,3 +47,14 @@ const perguntas = [
         ]
     }
 ];
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta(){ 
+    if (atual >= perguntas.length) {  
+        mostraResultado();
+        return;    
+}
+
